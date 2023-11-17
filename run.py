66c53da7b4496b49aadb96ab025f0b82a0ba4361
ThePyth0nKid <>
2023-11-17 14:32:03 +0000

    Add load_dictionary function to read words from files

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
 import random
+
+
+def load_dictionary(file_path):
+    """
+    Safely loads a list of words from a text file.
+    Handles file not found errors and empty file cases.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            words = [line.strip().lower() for line in file if line.strip()]
+        return words
+    except FileNotFoundError:
+        print(f"Error: The file {file_path} was not found.")
+        return []
+        
